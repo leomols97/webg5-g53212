617224fa4d5db7e36c93d01120d180ca82b9a80c
leomols97 <leomols@hotmail.com>
2022-09-19 17:36:16 +0200

    Now have to deploy on Heroku

---
+++ Chap6/hello/src/test/java/webg5/spring/hello/HelloApplicationTests.java
@@ -0,0 +1,12 @@
+package webg5.spring.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
